255ff9ac3c005d6bd18cc7317744b5fd733c3beb
Arcticflare <arcticflare69@gmail.com>
2020-04-29 17:34:00 +1200

    Consolidated files.

---
+++ MachineStatisticsOHM/ComputerStats.cs
@@ -0,0 +1,77 @@
+﻿using OpenHardwareMonitor.Hardware;
+using System;
+
+namespace MachineStatisticsOHM
+{
+    public class ComputerStats
+    {
+        public string CpuTemp { get; private set; }
+        public string CpuLoad { get; private set; }
+        public string GpuTemp { get; private set; }
+        public string GpuLoad { get; private set; }
+
+        public void GetComputerStats()
+        {
+            Computer comp = new Computer()
+            {
+                CPUEnabled = true,
+                GPUEnabled = true
+            };
+            comp.Open();
+
+            foreach (var hardware in comp.Hardware)
+            {
+                if(hardware.HardwareType == HardwareType.CPU)
+                {
+                    hardware.Update();
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            CpuTemp = sensor.Value.ToString();
+                        }
+
+                        if (sensor.SensorType == SensorType.Load)
+                        {
+                            CpuLoad = sensor.Value.ToString();
+                        }
+                    }
+                }
+
+                if (hardware.HardwareType == HardwareType.GpuAti)
+                {
+                    hardware.Update();
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            GpuTemp = sensor.Value.ToString();
+                        }
+
+                        if (sensor.SensorType == SensorType.Load)
+                        {
+                            GpuLoad = sensor.Value.ToString();
+                        }
+                    }
+                }
+
+                if (hardware.HardwareType == HardwareType.GpuNvidia)
+                {
+                    hardware.Update();
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            GpuTemp = sensor.Value.ToString();
+                        }
+
+                        if (sensor.SensorType == SensorType.Load)
+                        {
+                            GpuLoad = sensor.Value.ToString();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
